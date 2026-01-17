7e371dc26c8b82851d91c004ba8e169e511d8295
Swastik Patil <swastikpatil000@gmail.com>
2026-01-17 12:04:20 +0530

    Initial Commit

---
+++ Greenify/GreenifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenifyApp.swift
+//  Greenify
+//
+//  Created by Swastik Patil on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GreenifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
